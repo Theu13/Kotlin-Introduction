b1ce25970d6d6526df977149cc7eb226427e67e4
Theu13 <matheuslimabatista30@gmail.com>
2026-03-16 16:20:55 -0300

    .

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,59 @@
+
+fun main() {
+
+    // --- Variables ---
+    //println("--- Variables ---")
+
+
+    // String
+    var userName: String = "Jota" // changeable variable
+    val userName2: String = "Jao" // unique variable, not changeable
+
+    // Integer type
+    val age = 22
+    val integerMaxValue = Int.MAX_VALUE
+    val integerMinValue = Int.MIN_VALUE
+
+    val byteMaxValue = Byte.MAX_VALUE
+    val byteMinValue = Byte.MIN_VALUE
+
+    val longMaxValue = Long.MAX_VALUE
+    val longMinValue = Long.MIN_VALUE
+
+    //println("This is a String: $userName. This is an int: $age\n")
+
+    // Float and Double type
+    val myNumber: Float = 2.5f // float = f suffix | precision ~7
+    val pi = 3.14159265358979 // double | precision ~15
+
+    //println("This is a Float number: $myNumber, and this is a double number: $pi")
+
+
+
+    // Char and Boolean type
+    val myCharValue: Char = 'A'
+    //println("My char value is $myCharValue")
+
+    val myBooleanValue: Boolean = true
+    //println(myBooleanValue)
+    val BooleanTrue = true
+    val BooleanFalse = false
+    //println("Operators: &&, || and ! \nUsing BooleanTrue and BooleanFalse. \n&&: ${BooleanTrue && BooleanFalse} \n||: ${BooleanTrue || BooleanFalse} \n!: ${!BooleanTrue}")
+
+
+
+    // --- Operators ---
+    println("--- Operators ---")
+
+
+
+
+
+
+
+
+
+
+
+
+}
